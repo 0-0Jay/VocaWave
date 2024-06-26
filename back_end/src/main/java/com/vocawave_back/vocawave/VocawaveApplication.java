d33db733c1bb2eb014e543b1f5127d8dbafd8b3a
조영진 <117143398+0-0Jay@users.noreply.github.com>
2024-06-26 16:49:27 +0900

    Add files via upload

---
+++ back_end/src/main/java/com/vocawave_back/vocawave/VocawaveApplication.java
@@ -0,0 +1,13 @@
+package com.vocawave_back.vocawave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocawaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocawaveApplication.class, args);
+	}
+
+}
---
+++ back_end/src/test/java/com/vocawave_back/vocawave/VocawaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vocawave_back.vocawave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocawaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
