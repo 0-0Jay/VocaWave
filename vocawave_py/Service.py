49813575fdae3fb4955f7d2fbd53de8f1d4cee22
조영진 <117143398+0-0Jay@users.noreply.github.com>
2024-08-08 21:54:52 +0900

    Update Service.py

--- vocawave_py/Service.py
+++ vocawave_py/Service.py
@@ -29,19 +29,3 @@ def getScore(submit : dict, code : str) -> dict:
         rate = correct / len(score) * 100
         updateScore(code, rate)
     return score, ("%d" % rate)
-
-# def getRecommend(id : str):
-#     mylist = getMyWordlist(id)
-#     sharelist = getShareboard(id)
-#     keyword = []
-#     keyword += list(mylist['wtitle']) + list(mylist['cmt'])
-#     data = set()
-#     for word in keyword:
-#         data.add(re.sub(r'^\d+|\d+$', '', word))
-#     recommend = sharelist[sharelist.apply(lambda row: checkWord(row['title'], data) or checkWord(row['cmt'], data), axis=1)]
-#     return recommend
-
-# def checkWord(sentence, data):
-#     return any(word in sentence for word in data)
-
-    
