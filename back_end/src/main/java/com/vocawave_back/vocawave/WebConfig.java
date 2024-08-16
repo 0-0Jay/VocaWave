e81e3607212e42496a09368b604a4c728732f71d
조영진 <117143398+0-0Jay@users.noreply.github.com>
2024-08-16 19:06:20 +0900

    Add files via upload

--- back_end/src/main/java/com/vocawave_back/vocawave/WebConfig.java
+++ back_end/src/main/java/com/vocawave_back/vocawave/WebConfig.java
@@ -11,8 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry
                 .addMapping("/**") // 프로그램에서 제공하는 URL
                 .exposedHeaders("*")
-                .allowedOriginPatterns("http://localhost:8099",
-                        "http://localhost:3000") // 청을 허용할 출처를 명시, 전체 허용 (가능하다면 목록을 작성한다.
+                .allowedOriginPatterns("*") // 청을 허용할 출처를 명시, 전체 허용 (가능하다면 목록을 작성한다.
                 .allowedHeaders("*") // 어떤 헤더들을 허용할 것인지
                 .allowedMethods("*"); // 어떤 메서드를 허용할 것인지 (GET, POST...)
     }
